c088e5aa6fae00a7887572db488119b561f62de4
gustaaxz <gustavo_hatschbach@estudante.sesisenai.org.br>
2026-08-24 08:41:07 -0300

    feat: added comments to exer8

--- exercicios pdf/exer8.py
+++ exercicios pdf/exer8.py
@@ -14,3 +14,20 @@ print(f"Total produzido: {total}")
 print(f"Média de produção: {media:.2f}")
 print(f"Maior produção: {maior}")
 print(f"Menor produção: {menor}")
+
+"""
+def relatorio_producao(producao): // Define uma função passando o parâmetro producao
+    total = sum(producao) // Define o total produzido
+    media = total / len(producao) // Define a média produzida
+    maior = max(producao) // Define qual foi a maior produção
+    menor = min(producao) // Define qual foi a menor produção
+    return total, media, maior, menor // Retorna todos os valores
+
+producao = [850, 920, 880, 1050, 990] // Define os valores baseado nos parâmetros (total, media, maior, menor)
+
+print("--- Relatório de Produção ---")
+print(f"Total produzido: {total}")
+print(f"Média de produção: {media:.2f}")
+print(f"Maior produção: {maior}")
+print(f"Menor produção: {menor}")
+"""
