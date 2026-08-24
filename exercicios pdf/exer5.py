2e092407dc0f71cf547b9c06ef18ef93d2e18eeb
gustaaxz <gustavo_hatschbach@estudante.sesisenai.org.br>
2026-08-24 08:10:48 -0300

    feat: added comments to exer5

--- exercicios pdf/exer5.py
+++ exercicios pdf/exer5.py
@@ -9,3 +9,17 @@ realizada = int(input("Produção realizada: "))
 
 eficiencia = calcular_eficiencia(planejada, realizada)
 print(f"Eficiência: {eficiencia:.1f}%")
+
+"""
+def calcular_eficiencia(planejada, realizada): // Cria uma função onde calcula a eficiência da produção
+    if planejada <= 0:
+        return 0.0
+    eficiencia = (realizada / planejada) * 100
+    return eficiencia
+
+planejada = int(input("Produção planejada: "))
+realizada = int(input("Produção realizada: "))
+
+eficiencia = calcular_eficiencia(planejada, realizada) // Chama a função para calcular o valor dos dois números
+print(f"Eficiência: {eficiencia:.1f}%")
+"""
