10f478d43d2113b3088f3d83c002b1026d3631bb
gustaaxz <gustavo_hatschbach@estudante.sesisenai.org.br>
2026-08-24 08:04:09 -0300

    feat: added comments to exer3

--- exercicios pdf/exer3.py
+++ exercicios pdf/exer3.py
@@ -10,3 +10,18 @@ reprovadas = int(input("Peças reprovadas: "))
 
 percentual = calcular_percentual_aprovacao(produzidas, aprovadas, reprovadas)
 print(f"Percentual de aprovação: {percentual:.1f}%")
+
+"""
+def calcular_percentual_aprovacao(produzidas, aprovadas, reprovadas): // Cria uma função onde pega o percentual de peças produzidas, aprovadas e reprovadas
+    if produzidas <= 0:
+        return 0.0
+    percentual = (aprovadas / produzidas) * 100
+    return percentual
+
+produzidas = int(input("Peças produzidas: ))
+aprovadas = int(input("Peças aprovadas: ))
+reprovadas = int(input("Peças reprovadas: ))
+
+percentual = calcular_percentual_aprovacao(produzidas, aprovadas, reprovadas) // Chama a função para calcular o percentual de aprovação das peças
+print(f"Percentual de aprovação: {percentual:.1f}%") // Print com a formatação adequada
+"""
