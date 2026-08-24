92b26fabac04bbf238238fffb98667672f8640af
gustaaxz <gustavo_hatschbach@estudante.sesisenai.org.br>
2026-08-24 08:31:55 -0300

    feat: added comments to exer7

--- exercicios pdf/exer7.py
+++ exercicios pdf/exer7.py
@@ -7,3 +7,15 @@ if capacete == "s" and oculos == "s" and protetor == "s" and calcado == "s":
     print("\nAcesso liberado para a atividade.")
 else:
     print("\nAtenção: verifique os EPIs antes de iniciar a atividade.")
+
+"""
+capacete = input("Está utilizando capacete? (s/n): ").strip().lower() // Define o input para não ter espaçamentos e ser minúsculo
+oculos = input("Está utilizando óculos de proteção? (s/n)": ).strip().lower() // Define o input para não ter espaçamentos e ser minúsculo
+protetor = input("Está utilizando protetor auricular? (s/n): ").strip().lower() // Define o input para não ter espaçamentos e ser minúsculo
+calcado = input("Está utilizando calçado de segurança? (s/n): ").strip().lower() // Define o input para não ter espaçamentos e ser minúsculo
+
+if capacete == "s" and oculos == "s" and protetor == "s" and calcado == "s":
+    print("\nAcesso lbierado para atividade")
+else:
+    print("\nAtenção: verifique os EPIs antes de iniciar a atividade!)
+"""
