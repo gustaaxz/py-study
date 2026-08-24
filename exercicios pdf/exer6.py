1faf65388469a4c860b83816c3bc574ccf7eec5e
gustaaxz <gustavo_hatschbach@estudante.sesisenai.org.br>
2026-08-24 08:12:25 -0300

    feat: added comments to exer6

--- exercicios pdf/exer6.py
+++ exercicios pdf/exer6.py
@@ -8,3 +8,16 @@ else:
     status = "Manutenção necessária"
 
 print(f"Status da máquina: {status}")
+
+"""
+horas = float(input("Digite as horas de funcionamento da máquina: "))
+
+if horas <= 500:
+    status = "Operação normal"
+elif horas <= 1000:
+    status = "Programar manutenção"
+else:
+    status = "Manutenção necessária"
+
+print(f"Status da máquina: {status}")
+"""
